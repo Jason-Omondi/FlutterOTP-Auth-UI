c7d2b7fb2b2df781eb1e211cfe9fb30d1b318298
Jason-Omondi <jason.naboth@gmail.com>
2023-04-13 21:17:01 +0300

    update 5

--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -1,59 +1,81 @@
 import 'package:flutter/material.dart';
-
 import 'login_screen.dart';
 
-class OnBoardingPage extends StatelessWidget{
-  const OnBoardingPage({Key? key}) : super(key: key);
-
+class OnBoardingPage extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Scaffold(
-      body: SafeArea(
-        child: OnboardContent(
-          image: 'assets/images/bugs.png', description: 'Firing Up...', title: 'Onboarding...',
+  Widget build(BuildContext context) => SafeArea(
+        child: IntroductionScreen(
+          pages: [
+            PageViewModel(
+              title: 'A reader lives a thousand lives',
+              body: 'The man who never reads lives only one.',
+              image: buildImage('assets/ebook.png'),
+              decoration: getPageDecoration(),
+            ),
+            PageViewModel(
+              title: 'Featured Books',
+              body: 'Available right at your fingerprints',
+              image: buildImage('assets/readingbook.png'),
+              decoration: getPageDecoration(),
+            ),
+            PageViewModel(
+              title: 'Simple UI',
+              body: 'For enhanced reading experience',
+              image: buildImage('assets/manthumbs.png'),
+              decoration: getPageDecoration(),
+            ),
+            PageViewModel(
+              title: 'Today a reader, tomorrow a leader',
+              body: 'Start your journey',
+              footer: ButtonWidget(
+                text: 'Start Reading',
+                onClicked: () => goToHome(context),
+              ),
+              image: buildImage('assets/learn.png'),
+              decoration: getPageDecoration(),
+            ),
+          ],
+          done: Text('Read', style: TextStyle(fontWeight: FontWeight.w600)),
+          onDone: () => goToHome(context),
+          showSkipButton: true,
+          skip: Text('Skip'),
+          onSkip: () => goToHome(context),
+          next: Icon(Icons.arrow_forward),
+          dotsDecorator: getDotDecoration(),
+          onChange: (index) => print('Page $index selected'),
+          globalBackgroundColor: Theme.of(context).primaryColor,
+          skipFlex: 0,
+          nextFlex: 0,
+          // isProgressTap: false,
+          // isProgress: false,
+          // showNextButton: false,
+          // freeze: true,
+          // animationDuration: 1000,
         ),
-      ),
-    );
-  }
-  
-  
-  
-}
+      );
 
-class OnboardContent extends StatelessWidget {
-  const OnboardContent({
-    super.key, required this.image, required this.title, required this.description
-  });
-  final String image, title, description;
+  void goToHome(context) => Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (_) => LoginPage()),
+      );
 
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const Spacer(),
-        Image.asset(
-          image,
-          height: 250,
+  Widget buildImage(String path) =>
+      Center(child: Image.asset(path, width: 350));
+
+  DotsDecorator getDotDecoration() => DotsDecorator(
+        color: Color(0xFFBDBDBD),
+        //activeColor: Colors.orange,
+        size: Size(10, 10),
+        activeSize: Size(22, 10),
+        activeShape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(24),
         ),
-        const Spacer(),
-        Text(
-          title,
-          textAlign: TextAlign.center,
-          style: Theme.of(context).textTheme.headlineMedium?.copyWith(fontWeight: FontWeight.w500),
-        ),
-        const SizedBox(height: 25,),
+      );
 
-        Text(description,
-        textAlign: TextAlign.center,
-        ),
-        const Spacer(),
-
-        TextButton(onPressed: (){
-          Navigator.pushReplacement(context, MaterialPageRoute(builder: (BuildContext context) => LoginPage(),));
-        },
-         child: Text('Next', textAlign: TextAlign.center,))
-      ],
-    );
-  }
+  PageDecoration getPageDecoration() => PageDecoration(
+        titleTextStyle: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+        bodyTextStyle: TextStyle(fontSize: 20),
+        descriptionPadding: EdgeInsets.all(16).copyWith(bottom: 0),
+        imagePadding: EdgeInsets.all(24),
+        pageColor: Colors.white,
+      );
 }
