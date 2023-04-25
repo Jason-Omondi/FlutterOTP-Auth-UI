af3a3abac83c89f73f75bf11fba7ecaa1ab16835
Jason-Omondi <jason.naboth@gmail.com>
2023-04-25 08:07:31 +0300

    Update onboarding_screen.dart

--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -5,59 +5,65 @@ import 'login_screen.dart';
 
 class OnBoardingPage extends StatelessWidget {
   @override
-  Widget build(BuildContext context) => SafeArea(
-        child: IntroductionScreen(
-          pages: [
-            PageViewModel(
-              title: 'A reader lives a thousand lives',
-              body: 'In the bleak mid-winter',
-              image: buildImage('assets/images/kovu.png'),
-              decoration: getPageDecoration(),
-            ),
-            PageViewModel(
-              title: 'Here we go',
-              body: 'Available right at your fingerprints',
-              image: buildImage('assets/images/wada.png'),
-              decoration: getPageDecoration(),
-            ),
-            PageViewModel(
-              title: 'Simple UI Onboarding',
-              body: 'Just for enhanced app experience',
-              image: buildImage('assets/images/rr_.png'),
-              decoration: getPageDecoration(),
-            ),
-            PageViewModel(
-              title: 'Today a learner, tomorrow a beast',
-              body: 'Starting here',
-              footer: ButtonWidget(
-                text: 'Next steps',
-                onClicked: () => goToHome(context),
+  Widget build(BuildContext context){ 
+    return OrientationBuilder(
+      builder: (context, orientation) {
+        return SafeArea(
+            child: IntroductionScreen(
+              pages: [
+                PageViewModel(
+                  title: 'A reader lives a thousand lives',
+                  body: 'In the bleak mid-winter',
+                  image: buildImage('assets/images/kovu.png'),
+                  decoration: getPageDecoration(),
+                ),
+                PageViewModel(
+                  title: 'Here we go',
+                  body: 'Available right at your fingerprints',
+                  image: buildImage('assets/images/wada.png'),
+                  decoration: getPageDecoration(),
+                ),
+                PageViewModel(
+                  title: 'Simple UI Onboarding',
+                  body: 'Just for enhanced app experience',
+                  image: buildImage('assets/images/rr_.png'),
+                  decoration: getPageDecoration(),
+                ),
+                PageViewModel(
+                  title: 'Today a learner, tomorrow a beast',
+                  body: 'Starting here',
+                  footer: ButtonWidget(
+                    text: 'Next steps',
+                    onClicked: () => goToHome(context),
+                  ),
+                  image: buildImage('assets/images/cockpit_1.png'),
+                  decoration: getPageDecoration(),
+                ),
+              ],
+              done: Text('Read', style: TextStyle(fontWeight: FontWeight.w600, color: Colors.black54)),
+              onDone: () => goToHome(context),
+              showSkipButton: true,
+              skip: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('Skip', textAlign: TextAlign.center, style: TextStyle(color: Colors.black,),),
               ),
-              image: buildImage('assets/images/cockpit_1.png'),
-              decoration: getPageDecoration(),
+              onSkip: () => LoginPage(),
+              next: Icon(Icons.arrow_forward, color: Colors.yellow,),
+              dotsDecorator: getDotDecoration(),
+              onChange: (index) => print('Page $index selected'),
+              globalBackgroundColor: Theme.of(context).primaryColor,
+              skipFlex: 0,
+              nextFlex: 0,
+              // isProgressTap: false,
+              // isProgress: false,
+              // showNextButton: false,
+              // freeze: true,
+              // animationDuration: 1000,
             ),
-          ],
-          done: Text('Read', style: TextStyle(fontWeight: FontWeight.w600, color: Colors.black54)),
-          onDone: () => goToHome(context),
-          showSkipButton: true,
-          skip: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text('Skip', textAlign: TextAlign.center, style: TextStyle(color: Colors.black,),),
-          ),
-          onSkip: () => LoginPage(),
-          next: Icon(Icons.arrow_forward, color: Colors.yellow,),
-          dotsDecorator: getDotDecoration(),
-          onChange: (index) => print('Page $index selected'),
-          globalBackgroundColor: Theme.of(context).primaryColor,
-          skipFlex: 0,
-          nextFlex: 0,
-          // isProgressTap: false,
-          // isProgress: false,
-          // showNextButton: false,
-          // freeze: true,
-          // animationDuration: 1000,
-        ),
-      );
+          );
+      }
+    );
+  }
 
   void goToHome(context) => Navigator.of(context).pushReplacement(
         MaterialPageRoute(builder: (_) => LoginPage()),
